a5ce13e326a4b0b37cef3c0e28e32d289e40ff9d
Yash Teckchandani <yashteckchandani@Yashs-MacBook-Air.local>
2025-04-12 15:04:56 +0530

    dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () =>{
+    return (
+        <main>
+            <div>
+                <h1>Welcome to the React App</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App;
